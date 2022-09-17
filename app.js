56da641088a9a9fccd33958cc9ff920e0c2c5d86
Khire Green <khiregreen@Administrators-MacBook-Pro.local>
2022-09-16 20:12:28 -0400

    Final Cookie counter game

---
+++ app.js
@@ -0,0 +1,22 @@
+var name = prompt("What is your name?");
+
+//initial number of cookies    
+var num = 0;
+
+var cookie = document.getElementById("cookie");
+
+function cookieClick() { 
+    num += 1;
+
+    var numbers = document.getElementById("numbers");
+    
+    //upgrade level for printing
+    var upgradeLevel = document.getElementById("upgradeLevel");
+    
+    numbers.innerHTML = num;      
+    //automatic Granny upgrade to 2x
+    if(num >= 30 ){
+        num += 2;
+        upgradeLevel.innerHTML = "Granny";
+    }
+}
